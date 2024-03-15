f9dab4706248a2023ea561d3a8d2a21a52fadca2
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-03-15 11:11:33 -0600

    Using Hooks, adds text field input (a Task) to table of Tasks

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
+import {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 //import { Button } from 'react-native-web';
 
 export default function App() {
+
+  const [enteredToDoTask, setEnteredToDoTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  function taskTextEnteredHandler(text) {
+    setEnteredToDoTask(text);
+  }
+
+  function addTaskHandler() {
+    console.log('Task: ')
+    setTasks(t => [...t, enteredToDoTask]);
+  }
+
   return (
     <View style={styles.container}>
 
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='What do you need to do?'></TextInput>
+        <TextInput style={styles.textInput} placeholder='What do you need to do?' onChangeText={taskTextEnteredHandler}></TextInput>
         <View style={styles.addButton}>
-          <Button title='Add Task'/>
+          <Button onPress={addTaskHandler} title='Add Task'/>
         </View>
       </View>
 
       <View style={styles.tasksContainer}>
-        <Text>List of goals...</Text>
+        {tasks.map((t) => <Text>{t}</Text>)}
       </View>
 
     </View>
