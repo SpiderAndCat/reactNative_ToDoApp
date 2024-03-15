6df0e5d37e9b8f31beac60b157759b7e3d882bc9
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-03-15 10:41:49 -0600

    Basic layout with flexbox: task input fields

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+//import { Button } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='What do you need to do?'></TextInput>
+        <Button style={styles.addButton}title='Add Task'/>
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    flex: 1
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    marginRight: 16,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: 'blue',
+    width: '80%'
+
+  },
+  addButton: {
+    alignItems: 'center'
+  }
 });
